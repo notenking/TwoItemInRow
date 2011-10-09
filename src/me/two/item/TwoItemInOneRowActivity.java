43733e800b59f95bc6671eb6e32400b36d73bcfd
anddev <notenking@gmail.com>
2011-10-09 17:34:35 +0800

    实现横向两行，纵向三行，完成，但是感觉不是非常好

--- src/me/two/item/TwoItemInOneRowActivity.java
+++ src/me/two/item/TwoItemInOneRowActivity.java
@@ -3,132 +3,152 @@ package me.two.item;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AbsListView.OnScrollListener;
 
-public class TwoItemInOneRowActivity extends ListActivity {
+public class TwoItemInOneRowActivity extends ListActivity  {
 
 	private MyListAdapter myAdapter;
-    @SuppressWarnings("unchecked")
+	private String[] mStrings = new String[20];
+	
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        myAdapter = new MyListAdapter(this,"");
-        myAdapter.add("testa");
-        myAdapter.add("testb");
-        myAdapter.add("testc");
-        myAdapter.add("testd");
-        myAdapter.add("teste");
-        myAdapter.add("testf");
-        myAdapter.add("testg");
-        myAdapter.add("testh");
-        setListAdapter(myAdapter);
-    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		for (int i = 0; i < 20; i++) {
+			mStrings[i] = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+		}
+		myAdapter = new MyListAdapter(this, "");
+		
+		setListAdapter(myAdapter);
+	}
 
-	private class MyListAdapter extends ArrayAdapter {
-        /*public MyListAdapter(Context context) {
-            mContext = context;
-        }*/
+	private class MyListAdapter extends ArrayAdapter<String>  implements OnScrollListener{
+		private Context mContext;
+		/*
+		 * public MyListAdapter(Context context) { mContext = context; }
+		 */
 		public MyListAdapter(Activity activity, String channel) {
 			super(activity, 0);
 			mContext = (Context) activity;
-			
 			// initFooterView(activity);
 		}
+		public int getCount() {
+			if(mContext.getResources().getConfiguration().orientation==Configuration.ORIENTATION_LANDSCAPE){
+				return mStrings.length % 3  !=0 ? (mStrings.length / 3 + 1) : (mStrings.length / 3);
+			}
+				
+			return mStrings.length % 2 == 1 ? (mStrings.length / 2 + 1) : (mStrings.length / 2);
+			// return
+			// myAdapter.getCount()%2==1?(myAdapter.getCount()/2+1):(myAdapter.getCount()/2);
+		}
 
-        public int getCount() {
-            //return mStrings.length%2==1?(mStrings.length/2+1):(mStrings.length/2);
-           // return myAdapter.getCount()%2==1?(myAdapter.getCount()/2+1):(myAdapter.getCount()/2);
-        	return 4;
-        }
+		public View getView(final int position, View convertView, ViewGroup parent) {
+			Log.e("position=", "pos=" + position);
+			if(mContext.getResources().getConfiguration().orientation==Configuration.ORIENTATION_LANDSCAPE){
+				LinearLayout ll;
+				if (convertView == null) {
+					ll = (LinearLayout) LayoutInflater.from(mContext).inflate(R.layout.itemrow, parent, false);
+				} else {
+					ll = (LinearLayout) convertView;
+				}
 
-        /*@Override
-        public boolean areAllItemsEnabled() {
-            return false;
-        }
+				DontPressWithParentLinearLayout leftLayout = (DontPressWithParentLinearLayout) ll.findViewById(R.id.layout_left);
+				DontPressWithParentLinearLayout middleLayout = (DontPressWithParentLinearLayout) ll.findViewById(R.id.layout_middle);
+				DontPressWithParentLinearLayout rightLayout = (DontPressWithParentLinearLayout) ll.findViewById(R.id.layout_right);
 
-        @Override
-        public boolean isEnabled(int position) {
-            return !mStrings[position].startsWith("-");
-        }
-*/
-        public Object getItem(int position) {
-            return position;
-        }
+				TextView textLeft = (TextView) ll.findViewById(R.id.text_left);
+				TextView textRight = (TextView) ll.findViewById(R.id.text_right);
+				TextView textMiddle = (TextView) ll.findViewById(R.id.text_middle);
+				String testLeft = mStrings[3 * position].toString();
+				textLeft.setText(testLeft);
+				leftLayout.setOnClickListener(new OnClickListener() {
+					public void onClick(View arg0) {
+						Toast.makeText(TwoItemInOneRowActivity.this, "" + 3 * position, Toast.LENGTH_SHORT).show();
+					}
+				});
 
-        public long getItemId(int position) {
-            return position;
-        }
+				if (3 * position + 1 < mStrings.length) {
+					String testMiddle = mStrings[3 * position + 1].toString();
+					textMiddle.setText(testMiddle);
+					middleLayout.setOnClickListener(new OnClickListener() {
+						public void onClick(View arg0) {
+							Toast.makeText(TwoItemInOneRowActivity.this, "" + (3 * position + 1), Toast.LENGTH_SHORT).show();
+						}
+					});
+				} else {
+					textRight.setText("");
+				}
+				
+				if (3 * position + 2 < mStrings.length) {
+					String testRight = mStrings[3 * position + 2].toString();
+					textRight.setText(testRight);
+					rightLayout.setOnClickListener(new OnClickListener() {
+						public void onClick(View arg0) {
+							Toast.makeText(TwoItemInOneRowActivity.this, "" + (3 * position + 2), Toast.LENGTH_SHORT).show();
+						}
+					});
+				} else {
+					textRight.setText("");
+				}
+				return ll;
+			}else{
+			LinearLayout ll;
+			if (convertView == null) {
+				ll = (LinearLayout) LayoutInflater.from(mContext).inflate(R.layout.itemrow, parent, false);
+			} else {
+				ll = (LinearLayout) convertView;
+			}
 
-        public View getView(int position, View convertView, ViewGroup parent) {
-        	Log.e("position=","pos="+position);
-            LinearLayout ll;
-            if (convertView == null) {
-            	ll = (LinearLayout) LayoutInflater.from(mContext).inflate(
-                        R.layout.itemrow, parent, false);
-            } else {
-            	ll = (LinearLayout) convertView;
-            }
-            
-            DontPressWithParentLinearLayout leftLayout = (DontPressWithParentLinearLayout)ll.findViewById(R.id.layout_left);
-            DontPressWithParentLinearLayout rightLayout = (DontPressWithParentLinearLayout)ll.findViewById(R.id.layout_right);
-            leftLayout.setOnClickListener(null);
-            rightLayout.setOnClickListener(null);
-            TextView textLeft = (TextView)ll.findViewById(R.id.text_left);
-            TextView textRight = (TextView)ll.findViewById(R.id.text_right);
-            String testLeft=myAdapter.getItem(2*position).toString();
-            textLeft.setText(testLeft);
-            String testRight=myAdapter.getItem(2*position+1).toString();
-            if(2*position+1<myAdapter.getCount()){
-            textRight.setText(testRight);
-            }else{
-            	textRight.setText("");
-            }
-            return ll;
-        }
+			DontPressWithParentLinearLayout leftLayout = (DontPressWithParentLinearLayout) ll.findViewById(R.id.layout_left);
+			DontPressWithParentLinearLayout rightLayout = (DontPressWithParentLinearLayout) ll.findViewById(R.id.layout_right);
 
-        private Context mContext;
-    }
-    
-    private String[] mStrings = {
-            "----------",
-            "----------",
-            "Abbaye de Belloc",
-            "Abbaye du Mont des Cats",
-            "Abertam",
-            "----------",
-            "Abondance",
-            "----------",
-            "Ackawi",
-            "Acorn",
-            "Adelost",
-            "Affidelice au Chablis",
-            "Afuega'l Pitu",
-            "Airag",
-            "----------",
-            "Airedale",
-            "Aisy Cendre",
-            "----------",
-            "Allgauer Emmentaler",
-            "Alverca",
-            "Ambert",
-            "American Cheese",
-            "Ami du Chambertin",
-            "----------",
-            "----------",
-            "Anejo Enchilado",
-            "Anneau du Vic-Bilh",
-            "Anthoriro",
-            "----------",
-            "----------",
-            "----abcdef------"
-    };
+			TextView textLeft = (TextView) ll.findViewById(R.id.text_left);
+			TextView textRight = (TextView) ll.findViewById(R.id.text_right);
+			String testLeft = mStrings[2 * position].toString();
+			textLeft.setText(testLeft);
+			leftLayout.setOnClickListener(new OnClickListener() {
+				public void onClick(View arg0) {
+					Toast.makeText(TwoItemInOneRowActivity.this, "" + 2 * position, Toast.LENGTH_SHORT).show();
+				}
+			});
+
+			if (2 * position + 1 < mStrings.length) {
+				String testRight = mStrings[2 * position + 1].toString();
+				textRight.setText(testRight);
+				rightLayout.setOnClickListener(new OnClickListener() {
+					public void onClick(View arg0) {
+						Toast.makeText(TwoItemInOneRowActivity.this, "" + (2 * position + 1), Toast.LENGTH_SHORT).show();
+					}
+				});
+			} else {
+				textRight.setText("");
+			}
+			return ll;
+			}
+		}
+		
+		@Override
+		public void onScroll(AbsListView arg0, int arg1, int arg2, int arg3) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void onScrollStateChanged(AbsListView arg0, int arg1) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
 
 }
